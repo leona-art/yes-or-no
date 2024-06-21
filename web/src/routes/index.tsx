1ec443a5d8fd06fd77328c8057b85432d34f1d8f
leona-art <leonagraph@gmail.com>
2024-06-21 18:27:28 +0900

    refactor:

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { Title } from "@solidjs/meta";
 import { action, useAction, useSubmission } from "@solidjs/router";
-import { For, batch, createEffect, createSignal } from "solid-js";
+import { For, batch, createEffect, createMemo, createSignal } from "solid-js";
 import { ask as askGrpc } from "~/lib/grpc";
 
 const askAction = action(askGrpc)
@@ -8,26 +8,34 @@ const askAction = action(askGrpc)
 export default function Home() {
   const [question, setQuestion] = createSignal("")
   const asking = useSubmission(askAction)
-  const [history, setHistory] = createSignal<{
+  type AskRecord={
     question: string
     answer: string
-  }[]>([])
-
-  createEffect(() => {
+  }
+  const [history, setHistory] = createSignal<AskRecord[]>([])
+  
+  // const record=createMemo(()=>({
+  //   question: question(),
+  //   answer: asking.result ?? ""
+  // }))
+  createEffect<AskRecord[]>((prev) => {
     if (asking.result) {
-      batch(() => {
-        setHistory(history => [ {
-          question: question(),
-          answer: asking.result ?? ""
-        },...history])
-        asking.clear()
-      })
+      const record={
+        question: question(),
+        answer: asking.result
+      } satisfies AskRecord
+      setHistory([...prev,record].reverse())
+      asking.clear()
+      setQuestion("")
+      return [...prev,record]
+    }else{
+      return prev
     }
-  })
+  },[] as AskRecord[])
   return (
     <main>
       <Title>yes or no</Title>
-      <h1>yes or no</h1>
+      <h2>これは何でしょうか？</h2>
       <form action={askAction.with(question())} method="post">
         <input type="text" value={question()} onInput={e => setQuestion(e.currentTarget.value)} />
         <button type="submit">Ask</button>
